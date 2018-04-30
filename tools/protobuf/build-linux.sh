0491c0871bcc01133429c4158dec7998cf325087
gzhu <gary.zhu@sony.com>
2018-04-30 14:37:30 -0700

    Remove protobuf linux tar file

--- tools/protobuf/build-linux.sh
+++ tools/protobuf/build-linux.sh
@@ -2,7 +2,7 @@
 
 rm -rf linux
 
-if [ ! -f "protobuf-3.5.1" ]; then
+if [ ! -d "protobuf-3.5.1" ]; then
     if [ ! -f "protobuf-all-3.5.1.tar.gz" ]; then
         wget https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-all-3.5.1.tar.gz
     fi
