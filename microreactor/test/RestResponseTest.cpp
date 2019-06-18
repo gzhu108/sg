68eff3597e4817ad3ca5a7979c5d376ae0cca17d
Gary Zhu <gary.zhu@am.sony.com>
2019-06-18 13:06:46 -0700

    Fixed the RestResponseTest.

--- microreactor/test/RestResponseTest.cpp
+++ microreactor/test/RestResponseTest.cpp
@@ -7,12 +7,11 @@ using namespace sg::microreactor;
 TEST(RestResponseTest, ParseResponse)
 {
     auto message = std::make_shared<std::string>("HTTP/1.1 200 OK\r\nDate: Fri, 17 Aug 2018 20:46:47 GMT\r\n\r\n<html></html>");
+    std::shared_ptr<RestResponse> response = std::make_shared<RestResponse>();
+    RestMessage::Parse(response, message);
 
-    RestResponse response;
-    response.Parse(nullptr, message);
-
-    EXPECT_EQ(message, response.mRawMessage);
-    EXPECT_EQ(200, response.mStatusCode);
-    EXPECT_EQ("OK", response.mStatusText);
-    EXPECT_EQ("<html></html>", std::string(response.mBody.mOffset, response.mBody.mLength));
+    EXPECT_EQ(message, response->mRawMessage);
+    EXPECT_EQ(200, response->mStatusCode);
+    EXPECT_EQ("OK", response->mStatusText);
+    EXPECT_EQ("<html></html>", std::string(response->mBody.mOffset, response->mBody.mLength));
 }
